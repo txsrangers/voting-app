6e9a2bcfba621eab4f668e7d7b04b33eb80c7e22
Stephen Mayeux <stephenmayeux@gmail.com>
2017-02-20 10:02:21 -0600

    set up passport for authentication

---
+++ controllers/authentication.js
@@ -0,0 +1,38 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, 'appSecret');
+}
+
+exports.signin = function(req, res, next) {
+  res.send({ token: tokenForUser(req.user), user: req.user });
+}
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password'});
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+      res.json({ token: tokenForUser(user), user: user });
+    });
+  });
+}
